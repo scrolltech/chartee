71a74846acffe6a3ba511ef5475f30fd037be46a
reck <rakesh.gunduka@gmail.com>
2019-05-10 15:49:44 +0530

    Fixed a typo

--- be/app/endpoints.py
+++ be/app/endpoints.py
@@ -3,4 +3,4 @@ import be.app.libs.chart as chartlib
 
 def setup_routes(factory):
 	chart_handlers = (chartlib.list_, chartlib.create_and_publish, None, chartlib.get, chartlib.update_and_publish, chartlib.destroy)
-	factory.map_resource('/chart/', handlers=data_handlers)
+	factory.map_resource('/chart/', handlers=chart_handlers)
