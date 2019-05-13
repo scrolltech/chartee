427ef8671256dfa783cd11287fae4e878b1f182b
reck <rakesh.gunduka@gmail.com>
2019-05-13 16:49:36 +0530

    Updated endpoints

--- be/app/endpoints.py
+++ be/app/endpoints.py
@@ -2,6 +2,7 @@ import be.app.libs.chart as chartlib
 
 
 def setup_routes(factory):
-	chart_handlers = (chartlib.list_, chartlib.create_and_publish, None, chartlib.get, chartlib.update_and_publish, chartlib.destroy)
-	factory.map_resource('/chart/', handlers=chart_handlers)
-	factory.get('/chart/<int:id>/unpublish')(chartlib.unpublish)
+    factory.urls_prefix = '/api/1.0/'
+    chart_handlers = (chartlib.list_, chartlib.create_and_publish, None, chartlib.get, chartlib.update_and_publish, chartlib.destroy)
+    factory.map_resource('chart/', handlers=chart_handlers)
+    factory.get('/api/1.0/chart/{id}/unpublish')(chartlib.unpublish)
